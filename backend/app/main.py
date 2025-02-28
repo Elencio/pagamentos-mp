06e19e895c68d286f3dd99c10548fb40a3a8f8a3
elencio-dev <elencio.dev.studio@gmail.com>
2025-02-28 14:44:57 -0300

    Adicionando função seed_tipopagamentos para inserir registros iniciais de TipoPagamento (PIX e Boleto)

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,6 +7,25 @@ from . import models
 
 Base.metadata.create_all(bind=engine)
 
+def seed_tipopagamentos():
+    db: Session = SessionLocal()
+    try:
+        pix = db.query(models.TipoPagamento).filter(models.TipoPagamento.nome.ilike("PIX")).first()
+        if not pix:
+            pix = models.TipoPagamento(nome="PIX")
+            db.add(pix)
+        
+        boleto = db.query(models.TipoPagamento).filter(models.TipoPagamento.nome.ilike("Boleto")).first()
+        if not boleto:
+            boleto = models.TipoPagamento(nome="Boleto")
+            db.add(boleto)
+        
+        db.commit()
+    finally:
+        db.close()
+
+seed_tipopagamentos()
+
 app = FastAPI(title="API de Pagamentos")
 
 app.add_middleware(
